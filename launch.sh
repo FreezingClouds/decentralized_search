96d598d9b0f5e065f0ac4f1fd956e2c154c819dc
Jimmy Nguyen <austinnguyen517@berkeley.edu>
2020-12-06 15:41:40 -0800

    gitignore, bash ordering edit

--- launch.sh
+++ launch.sh
@@ -2,10 +2,10 @@
 cd ~/ros_workspaces/beeboop
 source devel/setup.bash
 roslaunch decentralized_search control_task_env.launch &
-sleep .1
-roslaunch decentralized_search pursuer_1.launch &
-sleep .1
+sleep .2
 roslaunch decentralized_search pursuer_2.launch &
-sleep .1
+sleep .2
 roslaunch decentralized_search pursuer_3.launch &
+sleep .2
+roslaunch decentralized_search evader.launch &
 wait
